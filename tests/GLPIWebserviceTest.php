74f38f38a7fec2a8c3407ef8d624dbb1aad84b1b
Vitor Bari Buccianti <vitorbari@gmail.com>
2017-04-10 18:42:16 -0300

    Add tests for unauthenticated methods

--- tests/GLPIWebserviceTest.php
+++ tests/GLPIWebserviceTest.php
@@ -10,15 +10,58 @@ class GLPIWebserviceTest extends \PHPUnit\Framework\TestCase
         return $serviceMock;
     }
 
-    public function testGetMyInfo()
+    public function testTest()
     {
         $serviceMock = $this->getServiceMock();
         $serviceMock->expects($this->once())->method('call')
             ->will($this->returnValue('output'));
 
-        $glpi = new VitorBari\GLPIWebservice\GLPIWebservice($serviceMock);
-        $glpi->auth('username', 'password');
-//        $output = $glpi->getMyInfo();
-//        $this->assertEquals('output', $output);
+        $glpi   = new VitorBari\GLPIWebservice\GLPIWebservice($serviceMock);
+        $output = $glpi->test();
+        $this->assertEquals('output', $output);
+    }
+
+    public function testStatus()
+    {
+        $serviceMock = $this->getServiceMock();
+        $serviceMock->expects($this->once())->method('call')
+            ->will($this->returnValue('output'));
+
+        $glpi   = new VitorBari\GLPIWebservice\GLPIWebservice($serviceMock);
+        $output = $glpi->status();
+        $this->assertEquals('output', $output);
+    }
+
+    public function testListAllMethods()
+    {
+        $serviceMock = $this->getServiceMock();
+        $serviceMock->expects($this->once())->method('call')
+            ->will($this->returnValue('output'));
+
+        $glpi   = new VitorBari\GLPIWebservice\GLPIWebservice($serviceMock);
+        $output = $glpi->listAllMethods();
+        $this->assertEquals('output', $output);
+    }
+
+    public function testListEntities()
+    {
+        $serviceMock = $this->getServiceMock();
+        $serviceMock->expects($this->once())->method('call')
+            ->will($this->returnValue('output'));
+
+        $glpi   = new VitorBari\GLPIWebservice\GLPIWebservice($serviceMock);
+        $output = $glpi->listEntities();
+        $this->assertEquals('output', $output);
+    }
+
+    public function testCountEntities()
+    {
+        $serviceMock = $this->getServiceMock();
+        $serviceMock->expects($this->once())->method('call')
+            ->will($this->returnValue(array('count' => 10)));
+
+        $glpi   = new VitorBari\GLPIWebservice\GLPIWebservice($serviceMock);
+        $output = $glpi->countEntities();
+        $this->assertEquals(10, $output);
     }
 }
